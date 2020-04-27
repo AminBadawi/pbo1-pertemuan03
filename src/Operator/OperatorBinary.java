f799d3b006f2f66c4e5eab9b62c37e718f4ff9bd
AminBadawi <aminbadawiofficial@gmail.com>
2020-04-27 12:46:53 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+
+    String nama                     = "Ahmad Amin Badawi";
+    int jumlahSKS                   = 24;
+    double indeksPrestasiKumulatif  = 3.90;
+
+}
